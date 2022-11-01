a9ff46b0ed267a971d690ec7c35e9bb23f358df4
tomeichlersmith <eichl008@umn.edu>
2022-11-01 09:36:50 -0500

    add comment pointing out the implicit double->int conversion

--- src/G4DarkBreM/ElementXsecCache.cxx
+++ src/G4DarkBreM/ElementXsecCache.cxx
@@ -31,6 +31,14 @@ void ElementXsecCache::stream(std::ostream& o) const {
 ElementXsecCache::key_t ElementXsecCache::computeKey(G4double energy,
                                                      G4double A,
                                                      G4double Z) const {
+  /**
+   * @note This implicitly converts the input double to a unsigned long int.
+   * Since the internal units for energy are MeV, this means the cache is
+   * binned at a 1 MeV level.
+   * The atomic inputs A and Z also undergo the implicit conversion; however,
+   * this is less of a worry since different elements are separated by at
+   * least one whole unit in A and Z.
+   */
   key_t energyKey = energy;
   key_t AKey = A;
   key_t ZKey = Z;
